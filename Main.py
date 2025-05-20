968c044cf2c6a6283b214d6239eca7fae8f78dd3
AliceVrot <165329287+AliceVrot@users.noreply.github.com>
2025-05-20 14:38:26 +0200

    changement main.py

--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
 #import numpy as np
 import pandas as pd
 #import matplotlib.pyplot as plt
-#import os
+import os
 #import math
 from TAC import TAC
 from conversionXLSXCSV import conversionXLSXCSV
@@ -25,13 +25,16 @@ import argparse
 # === ARGUMENTS DE LIGNE DE COMMANDE ===
 parser = argparse.ArgumentParser(description="Pipeline dosimétrie personnalisée")
 
-parser.add_argument("--data", required=True, help="Chemin vers le fichier .csv des données principales (DonneesGupta)")
+parser.add_argument("--data", required=True, help="Chemin vers le fichier .csv des données principales (DonneesEntre)")
 #parser.add_argument("--sval_annexe", required=True, help="Chemin vers le fichier des S-values (annexe)")
 parser.add_argument("--sval_fig5", required=True, help="Chemin vers les S-values de la figure 5")
 #parser.add_argument("--dose_cross", required=True, help="Chemin vers le fichier des doses absorbées (self+cross, Gupta)")
 #parser.add_argument("--dose_gupta", required=True, help="Chemin vers le fichier de comparaison dose Gupta et XieZaidi")
 parser.add_argument("--ainit", type=float, required=True, help="Activité initiale injectée (MBq)")
 parser.add_argument("--tphys", type=float, required=True, help="Temps physique du radionucléide (min)")
+parser.add_argument("--SortieActivite", type=float, required=True, help="1 (Oui) ou 0 (Non) pour la sortie de l'activité")
+parser.add_argument("--SortieDose", type=float, required=True, help="0(non), 1(dose par organe), 2 (matrice dose absorbée par les organes cible par organe source) pour la sortie de la dose absorbée")
+parser.add_argument("--EnregistrerSortieDose", type=str, required=True, help="0 ou 1 Ou nom du fichier de sortie")
 
 args = parser.parse_args()
 
@@ -39,42 +42,41 @@ Ainit = args.ainit
 Tphys = args.tphys
 
 # === CHARGEMENT DES DONNÉES ===
-DonneesGupta = pd.read_csv(args.data, index_col=0) #sheet_name = "Feuille1", index_col=0)
+DonneesEntre = pd.read_csv(args.data, index_col=0) #sheet_name = "Feuille1", index_col=0)
 #autredonnees= pd.read_xlsx(args.data, sheet_name = "Feuille2", index_col=0)
 #S_value_XieZaidi_annexe = pd.read_csv(args.sval_annexe, index_col=0)
-S_valueFigure5XieZaidi = pd.read_csv(args.sval_fig5, index_col=0)
+S_value = pd.read_csv(args.sval_fig5, index_col=0)
 #DoseAbsorbee_SelfCross_Gupta = pd.read_csv(args.dose_cross, index_col=0)
 #DoseAbsorbee_GuptaETXieZaidi = pd.read_csv(args.dose_gupta, index_col=0)
 
 #Tphys = autredonnees.loc[0, 'Tphys']
 #Ainit = autredonnees.loc[0, 'Ainit']
 
-organes = DonneesGupta.columns[6:16]
-print(organes, DonneesGupta)
+organes = DonneesEntre.columns[6:16]
 
 
 
+temps = DonneesEntre['Délais'].to_numpy()
 
-temps = DonneesGupta['Délais'].to_numpy()
-
-ActInt_corr, ActExt_corr, PartieExtrapolee, y_corr_matrice=TAC(DonneesGupta, temps, Ainit, Tphys)
+ActInt_corr, ActExt_corr, PartieExtrapolee, y_corr_matrice=TAC(DonneesEntre, temps, Ainit, Tphys)
 #print("ActInt: ", ActInt_corr*60, "ActExt: ", ActExt_corr*60)
 ActExt_corr=ActExt_corr*60
 ActInt_corr=ActInt_corr*60
 
 #print(ActExt_corr, ActInt_corr)
 #print(organes)
-DSelfAbsDose_annexe, DSelfAbsDose_figure5, SelfS_Value_XieZaidi_annexe, distribDose_parOrgane, DAbsTotale = CalculDoseSelf(ActInt_corr, ActExt_corr, organes, S_valueFigure5XieZaidi)#  S_value_XieZaidi_annexe, S_valueFigure5XieZaidi)
+DSelfAbsDose, SelfS_Value_XieZaidi_annexe, distribDose_parOrgane, DAbsTotale = CalculDoseSelf(ActInt_corr, ActExt_corr, organes, S_value)#  S_value_XieZaidi_annexe, S_valueFigure5XieZaidi)
 #print("après fin?")
 #print(DSelfAbsDose_figure5)
 
 
 
-DSelfAbsDose_annexe=DSelfAbsDose_annexe/Ainit
-DSelfAbsDose_figure5=DSelfAbsDose_figure5/Ainit
+DSelfAbsDose=DSelfAbsDose/Ainit
+#DSlfAbsDose_figure5=DSelfAbsDose_figure5/Ainit
 #print("DSelfAbsDose: ", DSelfAbsDose_annexe, "DSelfAbsDose_figure5: ", DSelfAbsDose_figure5)
 
-
+#organes = pd.Series(organes)
+#organes.to_csv("/home/verot/Projet/DonneesGupta/organes.csv")
 #Comparaison = comparaison_DoseAbsSelf_Biomaps_Gupta(organes, ActInt_corr, ActExt_corr, S_valueFigure5XieZaidi, Ainit, DSelfAbsDose_figure5, DSelfAbsDose_annexe, SelfS_Value_XieZaidi_annexe, DoseAbsorbee_SelfCross_Gupta, DoseAbsorbee_GuptaETXieZaidi)
 #print(Comparaison)
 #Comparaison.to_excel("/home/verot/Projet/Sorties/comparaisons.xlsx")
@@ -82,3 +84,37 @@ DSelfAbsDose_figure5=DSelfAbsDose_figure5/Ainit
 
 #plotActivite(temps, y_corr_matrice, PartieExtrapolee, organes)
 #barplotComparaison(DSelfAbsDose_annexe, DSelfAbsDose_figure5, 'titre', organes)
+
+
+
+Resultats = pd.DataFrame({
+        'Organes': organes,
+        'Activité Interpolée Corrigée (MBq.sec)': ActInt_corr,
+        'Activité Extrapolée Corrigée (MBq.sec)': ActExt_corr,
+        'Activité Accumulée Totale (MBq.sec)': (ActInt_corr + ActExt_corr),
+        #'Self Absorbed S-Value': S_value,
+        'Dose Absorbée': DAbsTotale,
+    })
+
+
+# === SORTIES ===
+if args.SortieActivite == 1:
+    #print('Activite interpolée: ', ActInt_corr, ' Activité extrapolée', ActExt_corr)
+    a=1
+
+if args.SortieDose == 1:
+    #print('Dose absorbée self (mGy.MBq⁻¹) Alice: ', DSelfAbsDose)
+    if args.EnregistrerSortieDose == 1:
+        DSelfAbsDose.to_csv("/home/verot/Projet/Sorties/DSelfAbsDose.csv")
+    #DSelfAbsDose.to_excel("/home/verot/Projet/Sorties/NOM.xlsx")
+if args.SortieDose == 2:
+    #print(distribDose_parOrgane)
+    if args.EnregistrerSortieDose == "1":
+        output_file = args.data.replace(".csv", "_resultats.csv")
+        Resultats.to_csv(output_file,header=True, index=True)
+
+    if args.EnregistrerSortieDose.endswith(".csv"):
+        output_file = args.EnregistrerSortieDose
+        Resultats.to_csv(output_file,header=True, index=True)
+
+    #distribDose_parOrgane.to_excel("/home/verot/Projet/Sorties/NOM.xlsx")
